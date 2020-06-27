588dde83291aae4abbc8f2d2e81f7e8eda18a53d
Elizabeth Crane <elizabeth.anne.crane@gmail.com>
2020-06-26 20:13:49 -0400

    task 4

--- index.js
+++ index.js
@@ -208,11 +208,19 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log("Task 1:");
 console.log(artists[0].name);
 console.log(artists[2].bio);
 
-/* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+/* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently 
+Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+
+artists[8].name = "Vincent Van Gogh";
+
+console.log(" ");
+console.log("Task 2:");
+console.log(artists[8].name);
 
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
@@ -224,19 +232,34 @@ console.log(artists[2].bio);
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+    return `The artist at index ${index} is ${array[index].name}`
   }
+
+  console.log(" ");
+  console.log("Task 3:");
+  console.log(getArtistByIndex(artists, 0));
   
   /**
 
 
-/* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
+/* Task 4: Create a function called get20s() that takes data as an argument and returns an array with 
+names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 
+1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
-
-  /* Code here */
-
+function get20s(array){
+  names = [];
+  for(i = 0; i < array.length; i++){
+    let yearArray = Array.from(array[i].years);
+    if(yearArray[0] === "1" && yearArray[1] === "9"){
+      names.push(array[i].name);
+    }
+  }
+  return names;
 }
+
+console.log(" ");
+console.log("Task 4:");
+console.log(get20s(artists));
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
